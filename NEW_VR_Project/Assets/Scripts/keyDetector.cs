7638d66e44da239ffd7343b2af9e1745f38b1c7c
Jaysus55 <114602010+Jaysus55@users.noreply.github.com>
2023-12-09 21:23:26 -0500

    script keypad

---
+++ NEW_VR_Project/Assets/Scripts/keyDetector.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyDetector : MonoBehaviour
+{
+   
+private TextMeshPro display;
+
+private KeyPadControll keyPadControll;
+
+    void Start()
+    {
+        display = GameObject.FindGameObjectWithTag("Display").GetComponentInChildren<TextMeshPro>();
+        display.text = "";
+
+        keyPadControll = GameObject.FindGameObjectWithTag("Keypad").GetComponent<KeyPadControll>();
+    }
+
+private void OnTriggerEnter(Collider other){
+
+if (other.CompareTag("KeypadButton")){
+
+var key = other.GetComponentInChildren<TextMeshPro>();
+if (key != null){
+    var keyFeedBack = other.gameObject.GetComponent<keyFeedBack>();
+
+    if (key.text == "Enter"){
+        var accessGranted = false;
+        if (display.text.Length > 0){
+            accessGranted = keyPadControll.CheckIfCorrect(Convert.ToInt32(display.text));
+        }
+if(accessGranted == true){
+   display.text = "Start"; 
+}else{
+    display.text = "Retry";
+}
+    }
+    else if (key.text == "Cancel"){
+        display.text = "";
+    }else{
+        bool onlyNumbers = int.TryParse(display.text, out int value);
+        if(onlyNumbers == false){
+            display.text = "";
+        }
+
+        if(display.text.Length < 4)
+        display.text += key.text;
+    }
+    keyFeedBack.keyHit = true;
+
+            }
+        }
+    }
+}
