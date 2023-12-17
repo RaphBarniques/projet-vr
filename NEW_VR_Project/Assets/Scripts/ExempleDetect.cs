95ffd96dff836519e3b4366ceb4a7039eafa7a70
KentCShit <94623626+RaphBarniques@users.noreply.github.com>
2023-12-17 18:21:36 -0500

    Added example script

---
+++ NEW_VR_Project/Assets/Scripts/ExempleDetect.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExempleDetect : MonoBehaviour
+{
+
+    public KeyPadControll keypadScript;
+    public KeycardReader keycardScript;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (keypadScript.accessGranted == true) {
+            //Do something
+        }
+
+        if (keycardScript.accessGranted == true) {
+            //Do something
+        }
+    }
+}
