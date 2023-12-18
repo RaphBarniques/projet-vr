661c57c46691fbbc7023fa92e2956c37ee89e69f
KentCShit <94623626+RaphBarniques@users.noreply.github.com>
2023-12-18 13:38:08 -0500

    Played with dongle

---
+++ NEW_VR_Project/Assets/Scripts/Dongle.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Dongle : MonoBehaviour
+{
+    public XRSocketInteractor xrSocket;
+    // Start is called before the first frame update
+    void Start()
+    {
+        xrSocket = GetComponent<XRSocketInteractor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public ReturnToSocket()
+    {   
+
+        if (xrSocket && xrSocket.selectTarget == null)
+        {
+            // Check if the object is not already in another socket
+            if (!IsInSocket())
+            {
+                // Move the object to the XR socket's position
+                transform.position = xrSocket.selectTarget.position;
+            }
+        }
+        
+    }
+
+    bool IsInSocket()
+    {
+        // Check if the object is a child of any XRSocketInteractor
+        XRSocketInteractor[] sockets = FindObjectsOfType<XRSocketInteractor>();
+        foreach (var socket in sockets)
+        {
+            if (socket != xrSocket && socket.selectTarget == transform)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
