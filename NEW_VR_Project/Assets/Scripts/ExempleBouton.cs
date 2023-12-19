06ab82c4e98ad8ebf2b8bc45fd7348555ea4a629
KentCShit <94623626+RaphBarniques@users.noreply.github.com>
2023-12-19 03:11:34 -0500

    Exemple button

---
+++ NEW_VR_Project/Assets/Scripts/ExempleBouton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExempleBouton : MonoBehaviour
+{
+    
+    public Button votreBouton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (votreBouton.isButtonPressed == true){
+            //Do something
+        }
+
+    }
+}
