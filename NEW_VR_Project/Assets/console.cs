94589924014389afba3c19de778d54da8235d400
KentCShit <94623626+RaphBarniques@users.noreply.github.com>
2023-12-19 08:34:44 -0500

    Puzzle Serveurs

---
+++ NEW_VR_Project/Assets/console.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class ListChildren : MonoBehaviour
+{
+    public List<Transform> childrenList = new List<Transform>();
+    public GameObject group;
+    public GameObject ErrorBox;
+    public TextMeshProUGUI Error;
+    public TextMeshProUGUI ButtonText;
+    public bool buttonClicked;
+    private string ErrorMessage;
+
+    void Start()
+    {
+
+        // Loop through all children and add them to the list
+        for (int i = 0; i < group.transform.childCount; i++)
+        {
+            Transform child = group.transform.GetChild(i);
+            childrenList.Add(child);
+        }
+        ErrorBox.SetActive(false);
+        StartCoroutine(WaitForButtonPress());
+    }
+
+    public void OnButtonClick(){
+        buttonClicked = true;
+    }
+
+    private IEnumerator WaitForButtonPress()
+    {   
+        buttonClicked = false;
+        yield return new WaitUntil(() => buttonClicked);
+        buttonClicked = false;
+        ButtonText.text = "Continuer";
+        for(int i = 0; i < 5; i++)
+        {   
+            int randomID = Random.Range(0,childrenList.Count);
+            serverRack server = childrenList[randomID].GetComponent<serverRack>();
+            
+            ErrorBox.SetActive(true);
+            ErrorMessage = "Erreur : Téléchargement bloqué. Redémarrer Serveur " + server.rackNumber + " pour continuer";
+            Error.text = ErrorMessage;
+            server.errorTrigger = true;
+            yield return new WaitUntil(() => server.buttonPressed);
+            ErrorBox.SetActive(false);
+            buttonClicked = false;
+            yield return new WaitUntil(() => buttonClicked);
+            buttonClicked = false;
+            yield return new WaitForSeconds(3f);
+        } 
+        
+    }
+}
