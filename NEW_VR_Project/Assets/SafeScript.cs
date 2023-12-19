3e09ed11111928791e5352a11b67a2ee0c72431e
joeballls <74332549+joeballls@users.noreply.github.com>
2023-12-18 21:21:18 -0500

    Safe script maybe works?

--- NEW_VR_Project/Assets/SafeScript.cs
+++ NEW_VR_Project/Assets/SafeScript.cs
@@ -1,13 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-/*
-public class ExempleDetect : MonoBehaviour
+public class SafeScript : MonoBehaviour
 {
 
     public KeyPadControll keypadScript;
-    public KeycardReader keycardScript;
+    public Rigidbody SafeDoor;
     
     // Start is called before the first frame update
     void Start()
@@ -20,12 +18,7 @@ public class ExempleDetect : MonoBehaviour
     {
         if (keypadScript.accessGranted == true) {
             //Do something
-        }
-
-        if (keycardScript.accessGranted == true) {
-            //Do something
-            animator.Play("ascenseur-porte");
+            SafeDoor.isKinematic = false;
         }
     }
 }
-*/
